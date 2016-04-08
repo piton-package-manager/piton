a84f76b828ffcea97bde4962858482231b789c19
looklikeapro <jerryzhou00@gmail.com>
2016-04-08 01:09:36 -0400

    0.1.1 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
 	name = 'piton',
 	license='LICENSE',
 	packages = ['piton', 'piton/utils', 'piton/commands'], # this must be the same as the name above
-	version = '0.1.0',
+	version = '0.1.1',
 	description = 'A local python package manager',
 	url = 'https://github.com/piton-package-manager/piton', # use the URL to the github repo
 	keywords = ['package', 'manager', 'local'], # arbitrary keywords
