97df82b78f139f24fd344e819c1cc374a22dcd72
looklikeapro <jerryzhou00@gmail.com>
2016-04-10 00:26:21 -0400

    specify version while installing

--- piton/commands/install.py
+++ piton/commands/install.py
@@ -20,20 +20,14 @@ class CommandInstall():
 				dependencies[package] = version_markup
 				package_json.write_dependencies(dependencies)
 		if not package:
-			installed_package_metadatas = python_modules.get_packages(os.path.join(os.getcwd(), "python_modules"))
-			installed_package_names = list(map(lambda md: md["name"], installed_package_metadatas))
-			dependencies = package_json.get_dependencies()
-			install_queue = []
-			for dependency in dependencies:
-				if not dependency in installed_package_names:
-					install_queue.append({
-						"name": dependency,
-						"version": dependencies[dependency]
-					})
-			for item in install_queue:
-				versions = list(map(lambda md: md["version"], pypi_api.get_avaliable_versions(item["name"])))
-				wanted = wanted_version(item["version"], versions)
-				cls.perform_install(item["name"], wanted)
+			cls.install_all()
+			return
+		if "@" in package:
+			split = package.split("@")
+			package_name = split[0]
+			package_version = split[1]
+			cls.perform_install(package_name, package_version)
+			package_json_if_save(save, "^"+package_version)
 		else:
 			latest_version = cls.install_latest(package)
 			if not latest_version:
@@ -65,3 +59,19 @@ class CommandInstall():
 		latest_version = sort_versions(versions)[-1:][0]
 		cls.perform_install(package, latest_version, True)
 		return latest_version
+	@classmethod
+	def install_all(cls):
+		installed_package_metadatas = python_modules.get_packages(os.path.join(os.getcwd(), "python_modules"))
+		installed_package_names = list(map(lambda md: md["name"], installed_package_metadatas))
+		dependencies = package_json.get_dependencies()
+		install_queue = []
+		for dependency in dependencies:
+			if not dependency in installed_package_names:
+				install_queue.append({
+					"name": dependency,
+					"version": dependencies[dependency]
+				})
+		for item in install_queue:
+			versions = list(map(lambda md: md["version"], pypi_api.get_avaliable_versions(item["name"])))
+			wanted = wanted_version(item["version"], versions)
+			cls.perform_install(item["name"], wanted)
