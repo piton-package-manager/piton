e176a1023ef5fb6efe2a6b041e9f31d02fca6563
looklikeapro <jerryzhou00@gmail.com>
2016-04-10 23:57:41 -0400

    clean up remove command

--- piton/commands/remove.py
+++ piton/commands/remove.py
@@ -5,19 +5,15 @@ class CommandRemove():
 	name = "remove"
 	@staticmethod
 	def decorate_subparser(subparser):
-		subparser.add_argument('program', type=str, nargs='?')
+		subparser.add_argument('programs', type=str, nargs='*')
 		subparser.add_argument("-s", "--save", action='store_true')
 	@classmethod
 	def run(cls, args):
-		cls._run(args.program, args.save)
-	@staticmethod
-	def _run(package, save):
-		removed = installer.remove(package)
-		if not removed:
-			print("package "+package+" is not installed")
-		if save:
-			package_json.remove_dependency(package)
-	@classmethod
-	def execute(cls, package):
-		# Code interface
-		cls._run(package, False)
+		if not args.programs:
+			return
+		for package in args.programs:
+			removed = installer.remove(package)
+			if not removed:
+				print("package "+package+" is not installed")
+			if args.save:
+				package_json.remove_dependency(package)
